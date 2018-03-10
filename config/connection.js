817a779a77d75bde2ead895fb7d880e126ecf938
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-10 02:02:01 -0600

    added jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
+
+
+    localhost: {
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+      },
+    
+      jawsDB: {
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+      }
 });
 
 connection.connect(function(err) {
